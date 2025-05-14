4910867454a9b76f5c85cef4b0a2914f464a3a4f
R.Aedo <renzo.aedo@gmail.com>
2025-05-14 13:44:21 -0400

    V012025MAY001.3 firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyDj4HZiGp3Ryp4dtaYRBKsyN9gdnx-x5F0",
+  authDomain: "alertcontrol-77d00.firebaseapp.com",
+  projectId: "alertcontrol-77d00",
+  storageBucket: "alertcontrol-77d00.firebasestorage.app",
+  messagingSenderId: "186220735145",
+  appId: "1:186220735145:web:5ce813fe45710970b657b8"
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';   
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment'; 
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
-});
+ if (environment.production) {
+enableProdMode();
+}
+
+ bootstrapApplication(AppComponent, {
+providers: [
+{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+provideRouter(routes),
+provideIonicAngular(),
+// Registra los providers de Firebase
+provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+provideAuth(() => getAuth()),
+// Agrega los providers de los otros módulos que necesites
+// provideFirestore(() => getFirestore()),
+// provideDatabase(() => getDatabase()),
+// provideStorage(() => getStorage()),
+// provideFunctions(() => getFunctions()),
+// provideMessaging(() => getMessaging()),
+// provideRemoteConfig(() => getRemoteConfig()),
+// provideAnalytics(() => getAnalytics()),
+// providePerformance(() => getPerformance())
+],
+}).catch((err) => console.log(err));
